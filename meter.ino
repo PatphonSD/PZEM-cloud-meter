8526f1216b6b1d5341cf0a99e772cc168c4790e4
พัชรพล ศรีดา <127929360+PatphonSD@users.noreply.github.com>
2023-09-04 18:39:36 +0700

    Update meter.ino

--- meter.ino
+++ meter.ino
@@ -10,28 +10,28 @@
 #define PZEM_TX_PIN 17
 #define PZEM_SERIAL Serial2
 #define NUM_PZEMS 5
+int START_INDEX = 11;
 #define ledPin 2
 
-const char* ssid = "farm";
-const char* password = "tas5630a";
-const char* serverUrl = "https://weregrin.com/v1/push_data/elect";
+const char *ssid = "Weregrin Meter";
+const char *password = "WeregrinX";
+const char *serverUrl = "https://weregrin.com/v1/push_data/elect";
 
 int timezone = 7 * 3600;
 int dst = 0;
-unsigned long previousMillis = 0;
 const long measureInterval = 500;
 
 PZEM004Tv30 pzems[NUM_PZEMS];
 LiquidCrystal_I2C lcd1(0x27, 20, 4);
 LiquidCrystal_I2C lcd2(0x26, 20, 4);
-sllib light1(ledPin);
+sllib LED(ledPin);
 
 unsigned long powerLoggerPreviousMillis = 0;
 unsigned long displayPowerDataPreviousMillis = 0;
 unsigned long postRequestPowerDataPreviousMillis = 0;
-const unsigned long powerLoggerInterval = 250;
+const unsigned long powerLoggerInterval = 500;
 const unsigned long displayPowerDataInterval = 1000;
-const unsigned long PostRequestPowerDataInterval = 30000;
+const unsigned long PostRequestPowerDataInterval = 300000;
 
 float voltage[NUM_PZEMS];
 float current[NUM_PZEMS];
@@ -40,45 +40,62 @@ float energy[NUM_PZEMS];
 float frequency[NUM_PZEMS];
 float pf[NUM_PZEMS];
 
-void setup() {
+void setup()
+{
   HardwareSetup();
   NetworkSetup();
   configTime(timezone, dst, "pool.ntp.org", "time.nist.gov");
-  getTime();
 }
 
-void loop() {
+void loop()
+{
+  LED.update();
   checkWifiConnection();
   unsigned long currentMillis = millis();
-  if (currentMillis - powerLoggerPreviousMillis >= powerLoggerInterval) {
+  if (currentMillis - powerLoggerPreviousMillis >= powerLoggerInterval)
+  {
     powerLoggerPreviousMillis = currentMillis;
     readPZEMValues();
-    for (int i = 0; i < NUM_PZEMS; i++) {
+    for (int i = 0; i < NUM_PZEMS; i++)
+    {
       PowerLogger(i, voltage[i], current[i], power[i], energy[i], frequency[i], pf[i]);
     }
   }
 
-  if (currentMillis - displayPowerDataPreviousMillis >= displayPowerDataInterval) {
+  if (currentMillis - displayPowerDataPreviousMillis >= displayPowerDataInterval)
+  {
     displayPowerDataPreviousMillis = currentMillis;
-    for (int i = 0; i < NUM_PZEMS; i++) {
-      if (!isnan(voltage[i]) && !isnan(current[i]) && !isnan(power[i]) && !isnan(energy[i]) && !isnan(frequency[i]) && !isnan(pf[i])) {
+    for (int i = 0; i < NUM_PZEMS; i++)
+    {
+      if (!isnan(voltage[i]) && !isnan(current[i]) && !isnan(power[i]) && !isnan(energy[i]) && !isnan(frequency[i]) && !isnan(pf[i]))
+      {
         displayPowerData(i, voltage[i], current[i], power[i], energy[i]);
       }
     }
   }
 
-  if (currentMillis - postRequestPowerDataPreviousMillis >= PostRequestPowerDataInterval) {
+  if (currentMillis - postRequestPowerDataPreviousMillis >= PostRequestPowerDataInterval)
+  {
     postRequestPowerDataPreviousMillis = currentMillis;
-    for (int i = 0; i < NUM_PZEMS; i++) {
-      if (!isnan(voltage[i]) && !isnan(current[i]) && !isnan(power[i]) && !isnan(energy[i]) && !isnan(frequency[i]) && !isnan(pf[i])) {
-        PostRequest(voltage[i], current[i], power[i], energy[i], frequency[i], pf[i], getTime(), i + 1);
+    for (int i = 0; i < NUM_PZEMS; i++)
+    {
+      if (!isnan(voltage[i]) && !isnan(current[i]) && !isnan(power[i]) && !isnan(energy[i]) && !isnan(frequency[i]) && !isnan(pf[i]))
+      {
+        sendHttpRequest(generateJsonPayload(voltage, current, power, energy, frequency, pf, getTime(), i + START_INDEX));
       }
     }
   }
 }
 
-void readPZEMValues() {
-  for (int i = 0; i < NUM_PZEMS; i++) {
+void FirstCore()
+{
+}
+
+void readPZEMValues()
+{
+  LED.breathSingle(1000);
+  for (int i = 0; i < NUM_PZEMS; i++)
+  {
     voltage[i] = pzems[i].voltage();
     current[i] = pzems[i].current();
     power[i] = pzems[i].power();
@@ -88,25 +105,39 @@ void readPZEMValues() {
   }
 }
 
-void PowerLogger(int i, float voltage, float current, float power, float energy, float frequency, float pf) {
-  Serial.print("PZEM ");
-  Serial.print(i);
-  Serial.print(" - Address:");
-  Serial.println(pzems[i].getAddress(), HEX);
+void PowerLogger(int i, float voltage, float current, float power, float energy, float frequency, float pf)
+{
+  Serial.print("Power meter ");
+  Serial.print(i + 1);
+  Serial.print(": ");
+  Serial.print(pzems[i].getAddress(), HEX);
   Serial.println("===================");
-  if (isnan(voltage)) {
+  if (isnan(voltage))
+  {
     Serial.println("Error reading voltage");
-  } else if (isnan(current)) {
+  }
+  else if (isnan(current))
+  {
     Serial.println("Error reading current");
-  } else if (isnan(power)) {
+  }
+  else if (isnan(power))
+  {
     Serial.println("Error reading power");
-  } else if (isnan(energy)) {
+  }
+  else if (isnan(energy))
+  {
     Serial.println("Error reading energy");
-  } else if (isnan(frequency)) {
+  }
+  else if (isnan(frequency))
+  {
     Serial.println("Error reading frequency");
-  } else if (isnan(pf)) {
+  }
+  else if (isnan(pf))
+  {
     Serial.println("Error reading power factor");
-  } else {
+  }
+  else
+  {
     Serial.print("Voltage: ");
     Serial.print(voltage);
     Serial.println("V");
@@ -129,8 +160,10 @@ void PowerLogger(int i, float voltage, float current, float power, float energy,
   Serial.println();
 }
 
-void displayPowerData(int i, float voltage, float current, float power, float energy) {
-  if (i > 3) {
+void displayPowerData(int i, float voltage, float current, float power, float energy)
+{
+  if (i > 3)
+  {
     lcd2.setCursor(0, i - 4);
     lcd2.print(String(voltage, 1));
     lcd2.print("V ");
@@ -138,7 +171,9 @@ void displayPowerData(int i, float voltage, float current, float power, float en
     lcd2.print("W ");
     lcd2.print(String(energy, 1));
     lcd2.print("U");
-  } else {
+  }
+  else
+  {
     lcd1.setCursor(0, i);
     lcd1.print(String(voltage, 1));
     lcd1.print("V ");
@@ -149,7 +184,8 @@ void displayPowerData(int i, float voltage, float current, float power, float en
   }
 }
 
-void HardwareSetup() {
+void HardwareSetup()
+{
   lcd1.init();
   lcd2.init();
   lcd1.backlight();
@@ -158,30 +194,38 @@ void HardwareSetup() {
   lcd2.home();
   lcd1.clear();
   lcd2.clear();
+  Serial.println("Setting up hardware...");
   Serial.begin(115200);
-  for (int i = 0; i < NUM_PZEMS; i++) {
+  for (int i = 0; i < NUM_PZEMS; i++)
+  {
     pzems[i] = PZEM004Tv30(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN, 0x1 + i);
   }
 }
 
-void NetworkSetup() {
-  light1.blinkSingle(500);
+void NetworkSetup()
+{
+  Serial.println("Setting up network...")
+  LED.blinkSingle(500);
   WiFiManager wifiManager;
   wifiManager.autoConnect(ssid, password);
   Serial.println("Connected to WiFi");
-  light1.setOffSingle();
+  LED.setOffSingle();
 }
 
-void checkWifiConnection() {
-  if (WiFi.status() != WL_CONNECTED) {
+void checkWifiConnection()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
     Serial.println("WiFi connection lost!");
     ESP.restart();
   }
 }
 
-String getTime() {
+String getTime()
+{
   struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
+  if (!getLocalTime(&timeinfo))
+  {
     Serial.println("Failed to obtain time");
     ESP.restart();
     return "";
@@ -191,44 +235,61 @@ String getTime() {
   return String(buffer);
 }
 
-void PostRequest(float volt, float curr, float powe, float energ, float freq, float pff, String dateTime, int id) {
-  String jsonPayload =
-    "{"
-    "\"voltage\": "
-    + String(volt) + ","
-                     "\"current\": "
-    + String(curr) + ","
-                     "\"power\": "
-    + String(powe) + ","
-                     "\"energy\": "
-    + String(energ) + ","
-                      "\"frequency\": "
-    + String(freq) + ","
-                     "\"pf\": "
-    + String(pff) + ","
-                    "\"datetime\": \""
-    + dateTime + "\","
-                 "\"meter_id\": "
-    + String(id) + "}";
+String generateJsonPayload(float voltage, float current, float power, float energy, float frequency, float pf, String dateTime, int id)
+{
+  // ตรวจสอบความถูกต้องของข้อมูล
+  if (isnan(voltage) || isnan(current) || isnan(power) || isnan(energy) || isnan(frequency) || isnan(pf))
+  {
+    Serial.println("Error: Invalid data");
+    return;
+  }
 
+  // สร้าง JSON payload
+  String jsonPayload =
+      "{"
+      "\"voltage\": " +
+      String(voltage) + ","
+                        "\"current\": " +
+      String(current) + ","
+                        "\"power\": " +
+      String(power) + ","
+                      "\"energy\": " +
+      String(energy) + ","
+                       "\"frequency\": " +
+      String(frequency) + ","
+                          "\"pf\": " +
+      String(pf) + ","
+                   "\"datetime\": \"" +
+      dateTime + "\","
+                 "\"meter_id\": " +
+      String(id) + "}";
+
+  return jsonPayload;
+}
+
+void sendHttpRequest(String jsonPayload)
+{
   HTTPClient http;
   http.begin(serverUrl);
   http.addHeader("Content-Type", "application/json");
-
   int httpResponseCode = http.POST(jsonPayload);
 
-  if (httpResponseCode > 0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    if (httpResponseCode == 500) {
-      Serial.println(jsonPayload);
+  if (httpResponseCode > 0)
+  {
+    // ตรวจสอบสถานะ HTTP ตอบกลับ
+    if (httpResponseCode != 200)
+    {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
     }
+
     String response = http.getString();
     Serial.println(response);
-  } else {
+  }
+  else
+  {
     Serial.print("Error in HTTP POST request. HTTP Response code: ");
     Serial.println(httpResponseCode);
-    Serial.println(jsonPayload);
   }
   http.end();
 }
